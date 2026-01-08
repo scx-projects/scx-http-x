1dfdc99d18781fb926747683e8ac2b7fa7b9289d
scx567888 <scx567888@outlook.com>
2026-01-08 16:24:48 +0800

    dev

--- src/main/java/dev/scx/http/x/HttpServerHelper.java
+++ src/main/java/dev/scx/http/x/HttpServerHelper.java
@@ -1,12 +1,5 @@
 package dev.scx.http.x;
 
-import dev.scx.function.Function1Void;
-import dev.scx.http.ScxHttpServerRequest;
-import dev.scx.http.error_handler.ScxHttpServerErrorHandler;
-import dev.scx.http.x.http1.Http1ServerConnection;
-import dev.scx.http.x.http1.Http1ServerConnectionOptions;
-import dev.scx.http.x.http2.Http2ServerConnection;
-import dev.scx.http.x.http2.Http2ServerConnectionOptions;
 import dev.scx.tcp.tls.TLS;
 
 import javax.net.ssl.SSLSocket;
@@ -40,34 +33,6 @@ final class HttpServerHelper {
     public static SSLSocket configServerTLS(Socket tcpSocket, TLS tls, BiFunction<SSLSocket, List<String>, String> protocolSelector) throws IOException {
         try {
             return configServerTLS0(tcpSocket, tls, protocolSelector);
-        } catch (IOException e) {
-            try {
-                tcpSocket.close();
-            } catch (IOException ex) {
-                e.addSuppressed(ex);
-            }
-            throw e;
-        }
-    }
-
-    /// 失败内部会关闭 Socket
-    public static Http1ServerConnection createHttp1ServerConnection(Socket tcpSocket, Http1ServerConnectionOptions options, Function1Void<ScxHttpServerRequest, ?> requestHandler, ScxHttpServerErrorHandler errorHandler) throws IOException {
-        try {
-            return new Http1ServerConnection(tcpSocket, options, requestHandler, errorHandler);
-        } catch (IOException e) {
-            try {
-                tcpSocket.close();
-            } catch (IOException ex) {
-                e.addSuppressed(ex);
-            }
-            throw e;
-        }
-    }
-
-    /// 失败内部会关闭 Socket
-    public static Http2ServerConnection createHttp2ServerConnection(Socket tcpSocket, Http2ServerConnectionOptions options, Function1Void<ScxHttpServerRequest, ?> requestHandler, ScxHttpServerErrorHandler errorHandler) throws IOException {
-        try {
-            return new Http2ServerConnection(tcpSocket, options, requestHandler, errorHandler);
         } catch (IOException e) {
             try {
                 tcpSocket.close();
