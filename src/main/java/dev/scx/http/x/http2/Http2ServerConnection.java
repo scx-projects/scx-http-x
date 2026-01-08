66bc39f4dea45dbb6efbaf1f29a7d87e5244bb4f
scx567888 <scx567888@outlook.com>
2026-01-08 16:31:47 +0800

    dev

--- src/main/java/dev/scx/http/x/http2/Http2ServerConnection.java
+++ src/main/java/dev/scx/http/x/http2/Http2ServerConnection.java
@@ -3,26 +3,17 @@ package dev.scx.http.x.http2;
 import dev.scx.function.Function1Void;
 import dev.scx.http.ScxHttpServerRequest;
 import dev.scx.http.error_handler.ScxHttpServerErrorHandler;
-import dev.scx.io.ByteInput;
-import dev.scx.io.ByteOutput;
+import dev.scx.http.x.SocketIO;
 
 import java.io.IOException;
-import java.net.Socket;
-
-import static dev.scx.io.ScxIO.createByteInput;
-import static dev.scx.io.ScxIO.createByteOutput;
 
 /// 占位 (可能永远都不会实现)
 public class Http2ServerConnection implements AutoCloseable {
 
-    private final Socket tcpSocket;
-    private final ByteInput dataReader;
-    private final ByteOutput dataWriter;
+    private final SocketIO socketIO;
 
-    public Http2ServerConnection(Socket tcpSocket, Http2ServerConnectionOptions options, Function1Void<ScxHttpServerRequest, ?> requestHandler, ScxHttpServerErrorHandler errorHandler) throws IOException {
-        this.tcpSocket = tcpSocket;
-        this.dataReader = createByteInput(this.tcpSocket.getInputStream());
-        this.dataWriter = createByteOutput(this.tcpSocket.getOutputStream());
+    public Http2ServerConnection(SocketIO socketIO, Http2ServerConnectionOptions options, Function1Void<ScxHttpServerRequest, ?> requestHandler, ScxHttpServerErrorHandler errorHandler) {
+        this.socketIO = socketIO;
     }
 
     public void start() {
@@ -31,7 +22,7 @@ public class Http2ServerConnection implements AutoCloseable {
 
     @Override
     public void close() throws IOException {
-        tcpSocket.close();
+        socketIO.close();
     }
 
 }
