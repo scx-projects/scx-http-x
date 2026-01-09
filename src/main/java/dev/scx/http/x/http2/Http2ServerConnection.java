81d4f4dd88b1734860cfd38b6bdb4021b5b00349
scx567888 <scx567888@outlook.com>
2026-01-09 17:38:46 +0800

    dev

--- src/main/java/dev/scx/http/x/http2/Http2ServerConnection.java
+++ src/main/java/dev/scx/http/x/http2/Http2ServerConnection.java
@@ -1,12 +1,12 @@
 package dev.scx.http.x.http2;
 
-import dev.scx.http.x.HttpServer;
+import dev.scx.http.x.HttpServerContext;
 import dev.scx.http.x.SocketIO;
 
 /// 占位 (可能永远都不会实现)
 public class Http2ServerConnection {
 
-    public static void start(SocketIO socketIO, HttpServer httpServer) {
+    public static void start(SocketIO socketIO, HttpServerContext context) {
         socketIO.closeQuietly();
         throw new UnsupportedOperationException("Not supported yet.");
     }
