7599224a1a0cf7c8da1472718db3c8eaacf0069c
scx567888 <scx567888@outlook.com>
2026-01-11 19:11:55 +0800

    dev

--- src/main/java/dev/scx/http/x/http1/Http1UpgradeHandler.java
+++ src/main/java/dev/scx/http/x/http1/Http1UpgradeHandler.java
@@ -1,14 +1,14 @@
 package dev.scx.http.x.http1;
 
 import dev.scx.http.ScxHttpServerRequest;
-import dev.scx.http.x.HttpServerContext;
-import dev.scx.http.x.SocketIO;
 import dev.scx.http.x.http1.headers.Http1Headers;
 import dev.scx.http.x.http1.headers.upgrade.ScxUpgrade;
 import dev.scx.http.x.http1.request_line.Http1RequestLine;
 import dev.scx.io.ByteInput;
 
 /// Http1UpgradeHandler
+///
+/// 本质上 可以看作一种 特殊的 RequestFactory
 ///
 /// @author scx567888
 /// @version 0.0.1
@@ -18,6 +18,6 @@ public interface Http1UpgradeHandler<T extends ScxHttpServerRequest> {
     ScxUpgrade upgradeProtocol();
 
     /// 创建升级之后的 Request
-    T createUpgradedRequest(Http1RequestLine requestLine, Http1Headers headers, ByteInput bodyByteInput, SocketIO socketIO, HttpServerContext context);
+    T createUpgradedRequest(Http1RequestLine requestLine, Http1Headers headers, ByteInput bodyByteInput, Http1ServerConnection connection);
 
 }
