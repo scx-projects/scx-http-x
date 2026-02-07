03100f3c0f0cfe2ca09523f6e9e17db9dd2f1328
scx567888 <scx567888@outlook.com>
2026-02-07 22:23:34 +0800

    dev

--- src/main/java/dev/scx/http/x/endpoint/SocketByteEndpoint.java
+++ src/main/java/dev/scx/http/x/endpoint/SocketByteEndpoint.java
@@ -1,5 +1,48 @@
 package dev.scx.http.x.endpoint;
 
-public class SocketByteEndpoint {
+import dev.scx.io.ByteInput;
+import dev.scx.io.ByteOutput;
+import dev.scx.io.endpoint.ByteEndpoint;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import static dev.scx.io.ScxIO.createByteInput;
+import static dev.scx.io.ScxIO.createByteOutput;
+
+/// SocketByteEndpoint
+///
+/// @author scx567888
+/// @version 0.0.1
+public final class SocketByteEndpoint implements ByteEndpoint {
+
+    public final Socket socket;
+    public final ByteInput in;
+    public final ByteOutput out;
+
+    public SocketByteEndpoint(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = createByteInput(this.socket.getInputStream());
+        this.out = createByteOutput(this.socket.getOutputStream());
+    }
+
+    @Override
+    public ByteInput in() {
+        return in;
+    }
+
+    @Override
+    public ByteOutput out() {
+        return out;
+    }
+
+    public Socket socket() {
+        return socket;
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.socket.close();
+    }
 
 }
